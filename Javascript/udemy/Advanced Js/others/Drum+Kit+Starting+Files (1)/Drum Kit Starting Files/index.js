582ceeddd237180e826e662be2cea347795390d4
Davi Tavares <twitchdavi072@gmail.com>
2026-02-18 18:53:25 -0300

    yes

--- Javascript/udemy/Advanced Js/others/Drum+Kit+Starting+Files (1)/Drum Kit Starting Files/index.js
+++ Javascript/udemy/Advanced Js/others/Drum+Kit+Starting+Files (1)/Drum Kit Starting Files/index.js
@@ -3,17 +3,14 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (let i = 0; i<numberOfDrumButtons; i++ ){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-   
+    this.style.color = "white"
     })
 
   var buttonInnerHTML = this.innerHTML;
   makeSound(buttonInnerHTML);
-  buttonAnimation(buttonInnerHTML); 
 }
-
 document.addEventListener("keypress", function(event){
     makeSound(event.key);
-      buttonAnimation(buttonInnerHTML);
 })
 
 
@@ -57,9 +54,4 @@ function makeSound(key){
   default:
     console.log(buttonInnerHTML);
   }
-}
-
-function buttonAnimation(currentKey){
-  var activeButton = document.querySelector(".h1"+ currentKey).classList;
-  activeButton.classList.add("pressed");
 }
