212e9c5df522774508f38880af5a402c3f01c58e
Davi Tavares <twitchdavi072@gmail.com>
2026-02-25 17:18:29 -0300

    w

---
+++ Javascript/udemy/JQuery/exercises/ANIMATION/ex2/index.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+
+    $(h3).click(function(){
+        $(".resposta").
+    })
+
+})
+
+
+
+
+
+
+
+//.next() no jQuery é utilizado para selecionar e navegar para o 
+//próximo elemento irmão imediatamente seguinte a um elemento selecionado na árvore DOM.
+
+//.slideToggle() do jQuery serve para alternar a visibilidade de um 
+ //elemento (mostrar/ocultar) com um efeito de deslizar para cima ou para baixo
+
+ //slideUp() no jQuery serve para ocultar elementos HTML selecionados deslizando-os para 
+ //cima, diminuindo sua altura gradualmente até desaparecerem
