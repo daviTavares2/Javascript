040648f6456a8a52dbfc5fb00e57149596fb2677
Davi Tavares <twitchdavi072@gmail.com>
2026-04-14 15:39:57 -0300

    Update index.js

--- Javascript/udemy/Node/3.2 HTTP request/index.js
+++ Javascript/udemy/Node/3.2 HTTP request/index.js
@@ -2,6 +2,18 @@ import express from "express";
 const app = express();
 const port = 1000;
 
+app.get("/",(req, res) => {
+    res.send("<h1>Hello</h1>");
+});
+ 
+app.get("/contact",(req,res) => {
+    res.send("<h1>My Phone:+55 (81) 998953122</h1>");
+});
+
+app.get("/about"(req,res) => {
+    res.send("<h1>About me</h1>");
+        res.send("<h2>My Name is David , i was born and raised in Brazil , since i was Kid i alredy was passionate about tech and video games.</h2>");
+});
 app.listen(port, ()=>{
 console.log(`server is running in port ${port}`);
-}) 
+});
