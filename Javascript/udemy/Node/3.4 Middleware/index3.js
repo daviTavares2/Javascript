a89112ed49c8114eb4423fc155d8d4bd2584bc76
Davi Tavares <twitchdavi072@gmail.com>
2026-04-21 20:38:36 -0300

    .

--- Javascript/udemy/Node/3.4 Middleware/index3.js
+++ Javascript/udemy/Node/3.4 Middleware/index3.js
@@ -1,7 +1,12 @@
 import express from "express";
-
 const app = express();
 const port = 3000;
+
+function logger(res, req, next) {
+  console.log("Request method : ",req.method);
+  console.log("Request URL: ",req.url);
+  next();
+}
 
 app.use(logger);
 
