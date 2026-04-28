b238f4afaf7341c13740b522d428e633e41e7bb9
Davi Tavares <twitchdavi072@gmail.com>
2026-04-27 21:45:07 -0300

    .

--- Javascript/udemy/EJS/4.0 EJS/index.js
+++ Javascript/udemy/EJS/4.0 EJS/index.js
@@ -1,15 +1,12 @@
-import express from "express";
+import express from "express"
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const app = express();
 const port = 3000;
-app.use(express.json());
+const app = express();
 
-
-
-app.listen(port, () => {
-console.log(`Listening on port ${port}`)
+app.listen(port, ()=>{
+console.log(`Server Listening on port ${port}`);
 })
