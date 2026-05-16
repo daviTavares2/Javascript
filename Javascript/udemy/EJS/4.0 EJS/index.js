8ab30bbe078778c8444daf42b8914854ee287cc6
Davi Tavares <twitchdavi072@gmail.com>
2026-05-15 21:31:31 -0300

    .

--- Javascript/udemy/EJS/4.0 EJS/index.js
+++ Javascript/udemy/EJS/4.0 EJS/index.js
@@ -7,6 +7,19 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = 3000;
 const app = express();
 
+
+
+
+app.get("/", (req, res) => {
+    const today = new Date();
+    const day = today.getDay();
+    console.log(day);
+    res.render("index.ejs",
+    { dayType: "a weekday",
+      advice: "its time to work  Hard",
+     })
+});
+
 app.listen(port, ()=>{
 console.log(`Server Listening on port ${port}`);
 })
